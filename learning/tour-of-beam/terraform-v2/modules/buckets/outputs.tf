bec03ed658a308320ca89fb1e6e800bdff3b8284
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2022-10-13 00:41:53 -0700

    Updated some tf sciprts

--- learning/tour-of-beam/terraform-v2/modules/buckets/outputs.tf
+++ learning/tour-of-beam/terraform-v2/modules/buckets/outputs.tf
@@ -1,7 +1,7 @@
-output "tf-state-bucket-id" {
-  value = google_storage_bucket.tf_state_bucket.id
-}
-
-output "tf-state-bucket-name" {
-  value = google_storage_bucket.tf_state_bucket.name
-}
+#output "tf-state-bucket-id" {
+#  value = google_storage_bucket.tf_state_bucket.id
+#}
+#
+#output "tf-state-bucket-name" {
+#  value = google_storage_bucket.tf_state_bucket.name
+#}
