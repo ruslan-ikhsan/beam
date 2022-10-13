dd816db78029f439be97f1868f424af6815aa33e
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2022-10-13 00:28:26 -0700

    Update main.tf

--- learning/tour-of-beam/terraform-v2/environments/uat/main.tf
+++ learning/tour-of-beam/terraform-v2/environments/uat/main.tf
@@ -8,12 +8,15 @@ provider "google" {
 
 module "iam" {
   source = "../../modules/iam"
+  project_id = var.project_id
 }
 
 module "buckets" {
   source = "../../modules/buckets"
+  project_id = var.project_id
 }
 
 module "api_enable" {
   source = "../../modules/api_enable"
+  project_id = var.project_id
 }
