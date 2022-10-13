2876c057340511b3f83ceae7d3e768970a2cc027
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2022-10-13 00:41:53 -0700

    Updated some tf sciprts

--- learning/tour-of-beam/terraform-v2/environments/uat/outputs.tf
+++ learning/tour-of-beam/terraform-v2/environments/uat/outputs.tf
@@ -2,10 +2,10 @@ output "service-account-email" {
   value = module.iam.service-account-email
 }
 
-output "tf-state-bucket-id" {
-  value = module.buckets.tf-state-bucket-id
-}
-
-output "tf-state-bucket-name" {
-  value = module.buckets.tf-state-bucket-name
-}
+#output "tf-state-bucket-id" {
+#  value = module.buckets.tf-state-bucket-id
+#}
+#
+#output "tf-state-bucket-name" {
+#  value = module.buckets.tf-state-bucket-name
+#}
