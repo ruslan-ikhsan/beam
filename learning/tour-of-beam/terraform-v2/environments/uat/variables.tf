2ad535ea1ae3ff46b685f0237828d935ec722cc6
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2022-10-13 13:45:24 -0700

    Updated

--- learning/tour-of-beam/terraform-v2/environments/uat/variables.tf
+++ learning/tour-of-beam/terraform-v2/environments/uat/variables.tf
@@ -7,7 +7,7 @@ variable "project_id" {
 
 variable "service_account" {
   description = "Name of SA to run Cloud Function"
-  default = "tob-bckend-sa"
+  default = "admin-sa"
 }
 
 variable "region" {
--- learning/tour-of-beam/terraform-v2/modules/iam/variables.tf
+++ learning/tour-of-beam/terraform-v2/modules/iam/variables.tf
@@ -4,6 +4,6 @@ variable "project_id" {
 
 variable "service_account_id" {
   description = "Name of SA to run Cloud Function"
-  default = "tob-bckend-sa"
+  default = "admin-sa"
 }
 
