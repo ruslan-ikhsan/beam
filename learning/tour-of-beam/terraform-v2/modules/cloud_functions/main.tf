d30d33d067857bc17aa9eafee9fe68b326cdfe02
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2022-10-14 02:34:26 -0700

    Updates

--- learning/tour-of-beam/terraform-v2/modules/cloud_functions/main.tf
+++ learning/tour-of-beam/terraform-v2/modules/cloud_functions/main.tf
@@ -3,10 +3,11 @@ resource "google_cloudfunctions_function" "cloud_function" {
   runtime               = "go116"
   project               = var.project_id
   region                = var.region
+  service_account_email = module.iam.service-account-email
   ingress_settings      = "ALLOW_ALL"
   # Get the source code of the cloud function as a Zip compression
-#  source_archive_bucket = module.buckets.functions-bucket-name
-#  source_archive_object = module.buckets.function-bucket-object
+  source_archive_bucket = module.buckets.functions-bucket-name
+  source_archive_object = module.buckets.function-bucket-object
 
   trigger_http = true
   # Name of the function that will be executed when the Google Cloud Function is triggered
@@ -23,5 +24,5 @@ module "iam" {
 module "buckets" {
   source = "../buckets"
   project_id = var.project_id
-  bucket_name = var.bucket_name
+  bucket_name = module.buckets.functions-bucket-name
 }
--- learning/tour-of-beam/terraform-v2/modules/cloud_functions/variables.tf
+++ learning/tour-of-beam/terraform-v2/modules/cloud_functions/variables.tf
@@ -1,9 +1,5 @@
 variable "region" {}
 variable "project_id" {}
-variable "bucket_name" {
-  description = "Bucket name to store function source code"
-}
-
 variable "service_account_id" {
   description = "Name of SA to run Cloud Function"
 }
