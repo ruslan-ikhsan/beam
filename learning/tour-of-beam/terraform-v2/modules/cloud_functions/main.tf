4068db649636b9280a2205439859472ddbf8cd6e
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2022-10-14 03:58:46 -0700

    Almost Final Version

--- learning/tour-of-beam/terraform-v2/modules/cloud_functions/main.tf
+++ learning/tour-of-beam/terraform-v2/modules/cloud_functions/main.tf
@@ -2,27 +2,13 @@ resource "google_cloudfunctions_function" "cloud_function" {
   name                  = "tour-of-beam-backend-cloud-function"
   runtime               = "go116"
   project               = var.project_id
+  service_account_email = var.service_account_id
+  source_archive_bucket = var.source_archive_bucket
+  source_archive_object = var.source_archive_object
   region                = var.region
-  service_account_email = module.iam.service-account-email
   ingress_settings      = "ALLOW_ALL"
   # Get the source code of the cloud function as a Zip compression
-  source_archive_bucket = module.buckets.functions-bucket-name
-  source_archive_object = module.buckets.function-bucket-object
-
   trigger_http = true
   # Name of the function that will be executed when the Google Cloud Function is triggered
   entry_point           = "init"
-
-}
-
-module "iam" {
-  source = "../iam"
-  service_account_id = var.service_account_id
-  project_id = var.project_id
-}
-
-module "buckets" {
-  source = "../buckets"
-  project_id = var.project_id
-  bucket_name = var.bucket_name
 }
--- learning/tour-of-beam/terraform-v2/modules/cloud_functions/variables.tf
+++ learning/tour-of-beam/terraform-v2/modules/cloud_functions/variables.tf
@@ -4,3 +4,6 @@ variable "service_account_id" {
   description = "Name of SA to run Cloud Function"
 }
 variable "bucket_name" {}
+variable "source_archive_bucket" {}
+
+variable "source_archive_object" {}
