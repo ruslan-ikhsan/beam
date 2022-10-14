c783e6ef959f35d3928cd5ad657e75ce184cc2ae
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2022-10-14 07:42:13 -0700

    Updates

--- learning/tour-of-beam/terraform-v2/modules/iam/main.tf
+++ learning/tour-of-beam/terraform-v2/modules/iam/main.tf
@@ -5,11 +5,11 @@ resource "google_service_account" "tourofbeam_cf_sa" {
 
 resource "google_project_iam_member" "terraform_service_account_roles" {
   for_each = toset([
-    "roles/resourcemanager.projectIamAdmin", "roles/cloudfunctions.developer", "roles/storage.objectViewer",
+    "roles/resourcemanager.projectIamAdmin", "roles/cloudfunctions.admin", "roles/storage.objectViewer",
     "roles/storage.objectCreator", "roles/iam.serviceAccountAdmin",
   ])
   role    = each.key
   member  = "serviceAccount:${google_service_account.tourofbeam_cf_sa.email}"
   project = var.project_id
 }
-
+g
