ddf645d46a1a04aa93fc4ce5cbc7ce397e00d82d
Damon Douglas <damondouglas@google.com>
2022-10-14 18:34:03 +0000

    WIP: begin royal refactoring implementation

---
+++ playground/infrastructure/01.setup/iam.tf
@@ -0,0 +1,28 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+resource "google_service_account" "service_account_iac" {
+  account_id = var.iac_service_account_id
+  display_name = var.iac_service_account_id
+  description = "The service account responsible for provisioning Google Cloud resources using Infrastructure-as-Code"
+}
+
+resource "google_service_account" "service_account_application" {
+  account_id = var.application_service_account_id
+  display_name = var.application_service_account_id
+  description = "The service account responsible for application workloads"
+}
