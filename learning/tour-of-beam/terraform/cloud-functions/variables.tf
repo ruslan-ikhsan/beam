5247d3acaacdcdb06f94c4f383dbe5d5ceb3c73b
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2022-10-16 13:57:28 -0700

    Delete redundant terraform dir

--- learning/tour-of-beam/terraform/cloud-functions/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "project_id" {
-  description = "The GCP Project ID where ToB Cloud Functions will be created"
-}
-
-variable "region" {
-  description = "Region of App"
-}
--- learning/tour-of-beam/terraform/setup/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project_id" {
-  description = "The GCP Project ID where ToB Cloud Functions will be created"
-}
-
-variable "service_account_id" {
-  description = "Service account ID"
-  default     = "tour-of-beam-sa"
-}
-
