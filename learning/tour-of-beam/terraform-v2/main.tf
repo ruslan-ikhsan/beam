7483e7f479534157cb34b3ddb250025bcf6b0f7c
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2022-10-21 03:08:25 -0700

    Final updates. Ready for review

--- learning/tour-of-beam/terraform-v2/main.tf
+++ learning/tour-of-beam/terraform-v2/main.tf
@@ -7,26 +7,26 @@ provider "google" {
 }
 
 module "iam" {
-  source = "modules/iam"
+  source = "./modules/iam"
   project_id = var.project_id
   service_account_id = var.service_account_id
   depends_on = [module.api_enable]
 }
 
 module "buckets" {
-  source = "modules/buckets"
+  source = "./modules/buckets"
   project_id = var.project_id
   bucket_name = var.bucket_name
   depends_on = [module.iam, module.api_enable]
 }
 
 module "api_enable" {
-  source = "modules/api_enable"
+  source = "./modules/api_enable"
   project_id = var.project_id
 }
 
 module "cloud_functions" {
-  source = "modules/cloud_functions"
+  source = "./modules/cloud_functions"
   region = var.region
   project_id = var.project_id
   bucket_name = var.bucket_name
--- learning/tour-of-beam/terraform-v2/variables.tf
+++ learning/tour-of-beam/terraform-v2/variables.tf
@@ -1,6 +1,7 @@
 variable "bucket_name" {
   description = "The bucket name that will store functions' source code"
 }
+
 variable "project_id" {
   type = string
   description = "The ID of the Google Cloud project within which resources are provisioned"
@@ -16,6 +17,7 @@ variable "service_account_id" {
   type = string
   description = "The ID of the service account responsible for running Google Cloud functions"
 }
+
 variable "region" {
   default = "us-central1"
 }
