b79398ab95743f3d7b43ee3e9a1744c974de06ea
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2022-11-07 17:58:57 +0500

    Update for PR 280

--- learning/tour-of-beam/terraform-v2/modules/api_enable/main.tf
+++ learning/tour-of-beam/terraform-v2/modules/api_enable/main.tf
@@ -1,27 +1,28 @@
-# Enable API for Cloud Build
-resource "google_project_service" "cloud_build" {
-  project = var.project_id
-  service = "cloudbuild.googleapis.com"
-  disable_on_destroy = false
-}
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
 
-# Enable API for Cloud Function
-resource "google_project_service" "cloud_function" {
-  project = var.project_id
-  service = "cloudfunctions.googleapis.com"
-  disable_on_destroy = false
-}
-
-# Enable API for Resource Manager
-resource "google_project_service" "resource_manager" {
-  project = var.project_id
-  service = "cloudresourcemanager.googleapis.com"
-  disable_on_destroy = false
-}
-
-# Enable API for IAM
-resource "google_project_service" "iam" {
-  project = var.project_id
-  service = "iam.googleapis.com"
+resource "google_project_service" "required_services" {
+  project   = var.project_id
+  for_each  = toset([
+    "cloudbuild",
+    "cloudfunctions",
+    "cloudresourcemanager",
+    "iam",
+    ])
+  service   = "${each.key}.googleapis.com"
   disable_on_destroy = false
 }
