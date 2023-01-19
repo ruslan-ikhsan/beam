b0660ba689cbda666e6a549c23a8b3fa77d5d217
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2023-01-19 22:43:49 +0500

    Update env_ci.sh

--- playground/infrastructure/env_ci.sh
+++ playground/infrastructure/env_ci.sh
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Comment
+# Added set -x to show output into cloud build console
 
 set -x
 
