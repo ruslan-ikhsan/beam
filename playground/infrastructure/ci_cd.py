dd5a6b531b2b7bafefac86e14e86ac91f2e2e35d
ruslan-ikhsan <ruslan.ikhsanov@akvelon.com>
2023-02-02 15:10:32 +0500

    Test tag

--- playground/infrastructure/ci_cd.py
+++ playground/infrastructure/ci_cd.py
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Comment
-
 """
 Module implements CI/CD steps for Beam Playground examples
 """
